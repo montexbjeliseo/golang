e1db5b95a44241a713552268645cae7c04853328
montexbjeliseo <secondmtx@gmail.com>
2022-11-10 01:03:15 -0300

    Return and handle an error

---
+++ Introduction/handle_error/my_handle_error.go
@@ -0,0 +1,15 @@
+package handle_error
+
+import (
+	"fmt"
+	"errors"
+)
+
+func SayHi(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Name is empty")
+	}
+
+	message := fmt.Sprintf("Hi %v. Welcome!", name)
+	return message, nil 
+}
